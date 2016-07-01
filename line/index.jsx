dd2eb8882838e7e7f1127b9b34c38ac2aa20513e
ledsun <shigeru.nakajima@gmail.com>
2016-07-01 10:54:40 +0900

    Update the Chart.js

--- line/index.jsx
+++ line/index.jsx
@@ -2,62 +2,131 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const {Line} = require("react-chartjs")
 
-const lineChartData = {
-  labels: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July"
-  ],
-  datasets: [
-    {
-      label: "My First dataset",
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ]
-    }, {
-      label: "My Second dataset",
-      fillColor: "rgba(151,187,205,0.2)",
-      strokeColor: "rgba(151,187,205,1)",
-      pointColor: "rgba(151,187,205,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(151,187,205,1)",
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+]
+
+const config = {
+  type: 'line',
+  data: {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July"
+    ],
+    datasets: [
+      {
+        label: "My First dataset",
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+        ],
+        fill: false,
+        borderDash: [5, 5]
+      }, {
+        hidden: true,
+        label: 'hidden dataset',
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+        ]
+      }, {
+        label: "My Second dataset",
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+        ]
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart'
+    },
+    tooltips: {
+      mode: 'label'
+    },
+    hover: {
+      mode: 'dataset'
+    },
+    scales: {
+      xAxes: [
+        {
+          display: true,
+          scaleLabel: {
+            show: true,
+            labelString: 'Month'
+          }
+        }
+      ],
+      yAxes: [
+        {
+          display: true,
+          scaleLabel: {
+            show: true,
+            labelString: 'Value'
+          },
+          ticks: {
+            suggestedMin: -10,
+            suggestedMax: 250
+          }
+        }
       ]
     }
-  ]
+  }
 }
 
-const option = {
-  responsive: true
+for (let dataset of config.data.datasets) {
+  dataset.borderColor = randomColor(0.4)
+  dataset.backgroundColor = randomColor(0.5)
+  dataset.pointBorderColor = randomColor(0.7)
+  dataset.pointBackgroundColor = randomColor(0.5)
+  dataset.pointBorderWidth = 1
 }
 
 ReactDOM.render(
-  <Line data={lineChartData} option={option} height="450" width="600"/>, document.querySelector('#container'))
+  <Line data={config.data} option={config.option}/>, document.querySelector('#container'))
 
 function randomScalingFactor() {
   return Math.round(Math.random() * 100)
+}
+
+function randomColorFactor() {
+  return Math.round(Math.random() * 255)
+}
+
+function randomColor(opacity) {
+  return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')'
 }
