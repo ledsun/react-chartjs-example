c8e531f72a0caab7cd8cb2cc89cb2d57fd82b5fb
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2016-07-04 00:09:13 +0900

    Add the Randomize Data Button

--- bar/index.jsx
+++ bar/index.jsx
@@ -18,58 +18,6 @@ const MONTHS = [
   "December"
 ]
 
-const data = {
-  labels: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July"
-  ],
-  datasets: [
-    {
-      label: 'Dataset 1',
-      backgroundColor: "rgba(220,220,220,0.5)",
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ]
-    }, {
-      hidden: true,
-      label: 'Dataset 2',
-      backgroundColor: "rgba(151,187,205,0.5)",
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ]
-    }, {
-      label: 'Dataset 3',
-      backgroundColor: "rgba(151,187,205,0.5)",
-      data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
-      ]
-    }
-  ]
-}
-
 const options = {
   // Elements options apply to all of the options unless overridden in a dataset
   // In this case, we are setting the border of each bar to be 2px wide and green
@@ -90,8 +38,88 @@ const options = {
   }
 }
 
+const App = React.createClass({
+  getInitialState() {
+    return {
+      labels: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July"
+      ],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          backgroundColor: "rgba(220,220,220,0.5)",
+          data: [
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor()
+          ]
+        }, {
+          hidden: true,
+          label: 'Dataset 2',
+          backgroundColor: "rgba(151,187,205,0.5)",
+          data: [
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor()
+          ]
+        }, {
+          label: 'Dataset 3',
+          backgroundColor: "rgba(151,187,205,0.5)",
+          data: [
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor(),
+            randomScalingFactor()
+          ]
+        }
+      ]
+    }
+  },
+  handleClick() {
+    const zero = Math.random() < 0.2
+      ? true
+      : false
+
+    const newData = this.state.datasets.map((dataset) => {
+      return {
+        backgroundColor: randomColor(),
+        data: dataset.data.map(function() {
+          return zero
+            ? 0.0
+            : randomScalingFactor()
+        })
+      }
+    })
+
+    this.setState({datasets: newData})
+  },
+  render() {
+    return <div>
+      <Bar data={this.state} options={options} ref={(ref) => this.Bar = ref}/>
+      <button onClick={this.handleClick}>Randomize Data</button>
+    </div>
+  }
+})
+
 ReactDOM.render(
-  <Bar data={data} options={options}/>, document.querySelector('#container'))
+  <App/>, document.querySelector('#container'))
 
 function randomScalingFactor() {
   return (Math.random() > 0.5
@@ -100,5 +128,5 @@ function randomScalingFactor() {
 }
 
 function randomColor(opacity) {
-  return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',.7)'
+  return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',0.7)'
 }
