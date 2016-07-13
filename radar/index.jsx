8505861854fd184f8967c290e7113d8c1d0635b6
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2016-07-13 09:39:06 +0900

    Add a radar chart sample

---
+++ radar/index.jsx
@@ -0,0 +1,98 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const {Radar} = require("react-chartjs")
+const randomColorFactor = require('../lib/randomColorFactor')
+
+const data = {
+  labels: [
+    "Eating",
+    "Drinking",
+    "Sleeping",
+    "Designing",
+    "Coding",
+    "Cycling",
+    "Running"
+  ],
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgba(220,220,220,0.2)",
+      pointBackgroundColor: "rgba(220,220,220,1)",
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ]
+    }, {
+      label: 'Hidden dataset',
+      hidden: true,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ]
+    }, {
+      label: "My Second dataset",
+      backgroundColor: "rgba(151,187,205,0.2)",
+      pointBackgroundColor: "rgba(151,187,205,1)",
+      hoverPointBackgroundColor: "#fff",
+      pointHighlightStroke: "rgba(151,187,205,1)",
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ]
+    }
+  ]
+}
+
+const options = {
+  legend: {
+    position: 'top'
+  },
+  title: {
+    display: true,
+    text: 'Chart.js Radar Chart'
+  },
+  scale: {
+    reverse: false,
+    gridLines: {
+      color: [
+        'black',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'indigo',
+        'violet'
+      ]
+    },
+    ticks: {
+      beginAtZero: true
+    }
+  }
+}
+
+ReactDOM.render(
+  <Radar data={data} options={options}/>, document.querySelector('#container'))
+
+function randomScalingFactor() {
+  return Math.round(Math.random() * 100)
+}
+
+function randomColor(opacity) {
+  return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')'
+}
